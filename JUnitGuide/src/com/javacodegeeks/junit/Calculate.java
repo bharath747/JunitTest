00c0fb5e0f8203da46c3ff51e1d8751b49c4a7d7
bharath <bharathsimha747@gmail.com>
2017-03-15 22:21:13 +0530

    Added Second Update to calculate

--- JUnitGuide/src/com/javacodegeeks/junit/Calculate.java
+++ JUnitGuide/src/com/javacodegeeks/junit/Calculate.java
@@ -4,6 +4,7 @@ public class Calculate {
 
 	public int sum(int var1, int var2) {
 		System.out.println("Claculate Updated");
+		System.out.println("Calculate Updated Second Time");
 		System.out.println("Adding values: " + var1 + " + " + var2);
 		return var1 + var2;
 	}
