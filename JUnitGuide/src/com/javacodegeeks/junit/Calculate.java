52bde95a3bce5df0efd946f03b4808a0489c55ac
bharath <bharathsimha747@gmail.com>
2017-03-15 21:31:02 +0530

    Added an update to Calculate

--- JUnitGuide/src/com/javacodegeeks/junit/Calculate.java
+++ JUnitGuide/src/com/javacodegeeks/junit/Calculate.java
@@ -3,6 +3,7 @@ package com.javacodegeeks.junit;
 public class Calculate {
 
 	public int sum(int var1, int var2) {
+		System.out.println("Claculate Updated");
 		System.out.println("Adding values: " + var1 + " + " + var2);
 		return var1 + var2;
 	}
