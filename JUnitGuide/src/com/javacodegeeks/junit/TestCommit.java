25ea8904ffe642ee78bf0660b4ee8c7679ce8a18
bharath747 <bharathsimha747@gmail.com>
2017-03-16 11:11:30 +0530

    Update TestCommit.java

--- JUnitGuide/src/com/javacodegeeks/junit/TestCommit.java
+++ JUnitGuide/src/com/javacodegeeks/junit/TestCommit.java
@@ -10,15 +10,17 @@ public class TestCommit
 		System.out.println("Added Method 2");
 		System.out.println("Inserted Method 1");
 		System.out.println("Inserted Method 4");
-        System.out.println("Inserted Method 3");
+                System.out.println("Inserted Method 3");
 		System.out.println("Inserted Method 3");
 		System.out.println("Inserted Method 5");
 		System.out.println("Added Method 8");
-        System.out.println("Inserted Method 3");
+                System.out.println("Inserted Method 3");
 		System.out.println("Inserted Method 3");
 		System.out.println("Added Method 6");
 		System.out.println("Inserted Method 5");
 		System.out.println("Added Method 7");
+		System.out.println("Completed");
+			
 	}
 
 }
