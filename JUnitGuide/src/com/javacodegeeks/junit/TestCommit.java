5ebfb653af460987abf5b089107858bd6ac88161
bharath <bharathsimha747@gmail.com>
2017-03-16 10:25:32 +0530

    Updated TestCommit

--- JUnitGuide/src/com/javacodegeeks/junit/TestCommit.java
+++ JUnitGuide/src/com/javacodegeeks/junit/TestCommit.java
@@ -9,11 +9,8 @@ public class TestCommit
 		System.out.println("Test Commit Updated by Other");
 		System.out.println("Added Method 2");
 		System.out.println("Inserted Method 1");
-<<<<<<< HEAD
 		System.out.println("Inserted Method 4");
-=======
 		System.out.println("Inserted Method 3");
->>>>>>> branch 'master' of https://github.com/bharath747/JunitTest.git
 	}
 
 }
